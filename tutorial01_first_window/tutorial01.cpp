98c16a16c6f54f7d8537d17fb463386922deeb0b
Jason Chen <shchenchang@gmail.com>
2019-04-23 15:02:09 +0800

    GLFW_OPENGL_FORWARD_COMPAT

--- tutorial01_first_window/tutorial01.cpp
+++ tutorial01_first_window/tutorial01.cpp
@@ -26,7 +26,7 @@ int main( void )
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     if (!(window = glfwCreateWindow(800, 600, "First Window", NULL, NULL)))
